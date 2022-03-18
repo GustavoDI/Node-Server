91f722066a65c260a145b09d169d770d1509bd7e
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2022-03-18 10:53:37 -0300

    Create DB CNN Mongoose

---
+++ database/config.db.js
@@ -0,0 +1,24 @@
+const mongoose = require ('mongoose');
+
+const dbCnn = async ()=>{
+
+    try {
+        await mongoose.connect( process.env.MONGODB_ATLAS, {
+            useNewUrlParser : true,
+            useUnifiedTopology: true,
+            // useCreateIndex: true, obsoleto
+            // useFindAndModify: false
+        });
+        console.log('Base datos en linea');
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error al iniciar de la base datos')
+    }
+
+
+}
+
+module.exports = {
+    dbCnn
+}
