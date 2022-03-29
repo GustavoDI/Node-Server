7c0bffe130cba5b3b2fcc60e679212fd008f3d94
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2022-03-29 08:51:36 -0300

    Middleware express-validator

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,13 +1,21 @@
 
 const {Router} = require('express') ;
+const { check } = require('express-validator');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios.controller');
 const router = Router();
+const {validarCampos} = require('../middlewares/validar-campos');
 
 router.get('/', usuariosGet);
 
 router.put('/:id', usuariosPut);
 
-router.post('/', usuariosPost);
+router.post('/',[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('correo', 'El correo no es valido').isEmail(),
+    check('password', 'El password es requerido').isLength({min:6}),
+    check('rol', 'No es un rol permitido').isIn(["ADMIN_ROLE","USER_ROLE"]),
+    validarCampos
+] ,usuariosPost);
 
 router.delete('/', usuariosDelete);
 
