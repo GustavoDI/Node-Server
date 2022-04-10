50cb354823cd0fe722f91e848cf5c85501a493a8
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2022-04-09 21:58:59 -0400

    Check en PUT

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -3,18 +3,24 @@ const {Router} = require('express') ;
 const { check } = require('express-validator');
 const router = Router();
 
+const {validarCampos} = require('../middlewares/validar-campos');
+const {esRolValido, emailExiste, existeUsuarioPorID}= require('../helpers/db-validator');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios.controller');
 
-const {esRolValido}= require('../helpers/db-validator')
-const {validarCampos} = require('../middlewares/validar-campos');
 
 router.get('/', usuariosGet);
 
-router.put('/:id', usuariosPut);
+router.put('/:id',[
+    check('id', 'No un ID válido').isMongoId(),
+    check('id',).custom(existeUsuarioPorID),
+    check('rol').custom(esRolValido),
+    validarCampos
+], usuariosPut);
 
 router.post('/',[
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('correo', 'El correo no es valido').isEmail(),
+    check('correo').custom(emailExiste),
     check('password', 'El password es requerido').isLength({min:6}),
     // check('rol', 'No es un rol permitido').isIn(["ADMIN_ROLE","USER_ROLE"]),
     check('rol').custom(esRolValido),
